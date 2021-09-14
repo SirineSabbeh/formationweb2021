5b0c4b190ff02b67058bcc88c9e373227323cbef
sirine sabbeh <sabbehsyrine@gmail.com>
2021-09-14 13:29:06 +0100

    formation web septembre 2021
    mon premier commit

---
+++ javascript udemy certification/basic.js
@@ -0,0 +1,8 @@
+document.write("hello we are learning javascript, hello i am instructor!");
+ var A=10;//integer
+ var b=15;//integer
+ var c=A+b;
+ var d="Hello world";//string
+ var e=false;//booleen
+ var g=10.5;//float
+ document.write(c);
